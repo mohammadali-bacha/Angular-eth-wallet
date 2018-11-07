e44402570f2b6965e2bb17f8c1beabd277a5aed2
m-ali <bacha@tuta.io>
2018-11-07 18:00:45 +0100

    add stepper

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatButtonModule } from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 
 
@@ -9,10 +10,10 @@ import {MatInputModule} from '@angular/material/input';
   declarations: [],
   imports: [
     CommonModule,
-    MatButtonModule, MatInputModule
+    MatButtonModule, MatInputModule,
   ],
   exports: [
-    MatButtonModule, MatInputModule
+    MatButtonModule, MatInputModule,
   ],
 })
 export class UiModule { }
